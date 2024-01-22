3348a215de57ffffa6403fe523b0584e3d4b4054
Josiah Wilkins <lowgen0@gmail.com>
2024-01-22 17:11:39 -0500

    update test so that they run

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,18 @@
-import { render, fireEvent, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, waitFor } from "@testing-library/react";
+import App from "./App";
+import userEvent from "@testing-library/user-event";
 
-test('adds a todo item', () => {
+test("adds a todo item", async () => {
+  const user = userEvent.setup();
   render(<App />);
-  
-  const inputElement = screen.getByRole('textbox');
-  const buttonElement = screen.getByRole('button');
-  
-  fireEvent.change(inputElement, { target: { value: 'Test Todo' } });
-  fireEvent.click(buttonElement);
-  
-  const todoElement = screen.getByText('Test Todo');
-  
-  expect(todoElement).toBeInTheDocument();
+
+  const inputElement = screen.getByRole("textbox");
+  const buttonElement = screen.getByRole("button");
+
+  await waitFor(async () => {
+    await user.type(inputElement, "Test Todo");
+    await user.click(buttonElement);
+    const todoElement = await screen.findByText(/Test Todo/);
+    expect(todoElement).toBeVisible();
+  });
 });

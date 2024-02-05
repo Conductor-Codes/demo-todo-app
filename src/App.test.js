8dc6553660485a290e30475ddfed08e530810f69
akogagarin <131894560+akogagarin@users.noreply.github.com>
2024-02-05 15:31:47 -0600

    add a delete button

--- src/App.test.js
+++ src/App.test.js
@@ -6,13 +6,37 @@ test("adds a todo item", async () => {
   const user = userEvent.setup();
   render(<App />);
 
-  const inputElement = screen.getByRole("textbox");
-  const buttonElement = screen.getByRole("button");
+  the inputElement = screen.getByRole("textbox");
+  const addButtonElement = screen.getByText("Add Todo");
 
   await waitFor(async () => {
     await user.type(inputElement, "Test Todo");
-    await user.click(buttonElement);
+    await user.click(addButtonElement);
     const todoElement = await screen.findByText(/Test Todo/);
     expect(todoElement).toBeVisible();
+  });
+});
+
+test("deletes a todo item", async () => {
+  const user = userEvent.setup();
+  render(<App />);
+
+  const inputElement = screen.getByRole("textbox");
+  const addButtonElement = screen.getByText("Add Todo");
+
+  // Add a todo item
+  await waitFor(async () => {
+    await user.type(inputElement, "Todo to Delete");
+    await user.click(addButtonElement);
+    const todoElement = await screen.findByText(/Todo to Delete/);
+    expect(todoElement).toBeVisible();
+  });
+
+  // Delete the added todo item
+  const deleteButtonElement = await screen.findByText("Delete");
+  await user.click(deleteButtonElement);
+
+  await waitFor(async () => {
+    expect(screen.queryByText(/Todo to Delete/)).toBeNull();
   });
 });

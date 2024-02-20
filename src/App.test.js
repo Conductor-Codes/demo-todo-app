0dfcae6758b402584554c1f5330bd415f8ee3e55
Raymond Lam <raymondlam1991@gmail.com>
2024-02-20 10:57:25 -0500

    Add the capability to delete a todo

--- src/App.test.js
+++ src/App.test.js
@@ -16,3 +16,29 @@ test("adds a todo item", async () => {
     expect(todoElement).toBeVisible();
   });
 });
+
+test("deletes a todo item", async () => {
+  const user = userEvent.setup();
+  render(<App />);
+
+  // Add a todo item first
+  const inputElement = screen.getByRole("textbox");
+  const addButtonElement = screen.getByRole("button", { name: /add todo/i });
+
+  await user.type(inputElement, "Todo to be deleted");
+  await user.click(addButtonElement);
+
+  // Verify the todo item was added
+  const todoElement = await screen.findByText(/Todo to be deleted/);
+  expect(todoElement).toBeVisible();
+
+  // Assuming a delete button is added next to each todo item for deletion
+  // This part of the test will fail until the delete functionality is implemented
+  const deleteButtonElement = screen.getByRole("button", { name: /delete/i });
+  await user.click(deleteButtonElement);
+
+  // Verify the todo item was deleted
+  await waitFor(() => {
+    expect(screen.queryByText(/Todo to be deleted/)).not.toBeInTheDocument();
+  });
+});
